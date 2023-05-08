77ea9bc9f1a9e1f04ad1c3eabb29951e8c6986df
korqemer <nmiras99@gmail.com>
2023-05-09 04:51:57 +0600

    Initial commit

---
+++ player.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+class Player (pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.Surface((80, 80))
+        self.image.load()
+        self.rect = self.image.get_rect(topleft = pos)
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            pass
+        elif keys[pygame.K_LEFT]:
+            pass
+        else:
+            pass
---
+++ settings.py
@@ -0,0 +1,16 @@
+level_map = [
+    '   X    X     ',
+    ' XX   XXXXX   ',
+    'XX      XXXX  ',
+    'XXXXXX   X    ',
+    '  XX    XX  XX',
+    '      XXXXXXXX',
+    'XXXX          ',
+    'XXXXXXXXX     ',
+    'XXXX     XXX  ',
+    'FW            '
+]
+
+tile_size = 80
+screen_width = 1200
+screen_height = len(level_map) * tile_size
